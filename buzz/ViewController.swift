5c0ad6f695c22f3a3c678b96042d8dc66869db13
mekchi <mekchi@mekchis-mac.local>
2016-08-09 15:12:10 +0600

    Initial Commit

---
+++ buzz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  buzz
+//
+//  Created by mekchi on 8/9/16.
+//  Copyright © 2016 mekchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
